53ab30f70a7d16fbbf8a72acec554ea48e272c04
joestephens <joe.stephens@live.co.uk>
2016-07-25 23:23:08 +0100

    Further comments

--- airportchallenge/airport.py
+++ airportchallenge/airport.py
@@ -1,8 +1,8 @@
-class Airport(object):
+class Airport(object): # inherits from the 'object' class
 
     def __init__(self, weather):
         self.planes = []
-        self.weather = weather
+        self.weather = weather # dependency injected weather
 
     def land(self, plane):
         if not self.weather.is_stormy():
